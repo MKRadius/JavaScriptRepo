cc7894c42f1a8c93f4de6ba030e32340e7a3132a
MKRadius <caomanhkhai110@gmail.com>
2022-11-16 22:25:28 +0200

    First commit

---
+++ JS1.js
@@ -0,0 +1,26 @@
+/*document.write("Exercise 1.1" + "<br>")
+
+console.log("I'm printing to console!")
+document.write("Text written in console" + "<br><br>")
+
+
+
+document.write("Exercise 1.2" + "<br>")
+let user_name = prompt("Enter your name")
+document.write(`Hello, ${user_name}  <br><br>`)
+
+
+
+document.write("Exercise 1.3" + "<br>")
+a = parseInt(prompt("Input value of a"))
+b = parseInt(prompt("Input value of b"))
+c = parseInt(prompt("Input value of c"))
+
+document.write(`a = ${a}, b = ${b}, c = ${c} <br>`)
+document.write("Sum = ", a + b + c, "<br>")
+document.write("Product = ", a * b * c, "<br>")
+document.write("Average = ", (a + b + c) / 3, "<br>")
+*/
+
+
+document.write("Exercise 1.4" + "<br>")
