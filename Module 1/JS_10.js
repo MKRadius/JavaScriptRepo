266998b1bc488d557ddcb509b13060dd5282c76a
MKRadius <caomanhkhai110@gmail.com>
2022-11-17 13:05:28 +0200

    Exercise

--- Module 1/JS_10.js
+++ Module 1/JS_10.js
@@ -1,24 +1,24 @@
 document.write("Exercise 10<br><br>");
 
-let dices, sum_dices;
+let number_of_dices, sum_of_dices;
 let counter = 0;
 let comp_dice_num, comp_sum_eyes;
 
-dices = parseInt(prompt("How many dice do you want to roll?"));
-sum_dices = parseInt(prompt("Sum of the eyes?"));
+number_of_dices = parseInt(prompt("How many dice do you want to roll?"));
+sum_of_dices = parseInt(prompt("Sum of the eyes?"));
 
 for (i = 0; i < 10000; i++) {
     comp_dice_num = 0;
     comp_sum_eyes = 0;
 
-    for (let j = 0; j < dices; j++) {
+    for (let j = 0; j < number_of_dices; j++) {
         comp_dice_num = Math.floor(Math.random() * 6 + 1);
         comp_sum_eyes += comp_dice_num;
     }
 
-    if (comp_sum_eyes === sum_dices) {
+    if (comp_sum_eyes === sum_of_dices) {
         counter++;
     }
 }
 
-document.write(`Probability to get sum ${sum_dices} with ${dices} dices is ${(counter / 10000 * 100).toFixed(2)}%`)
+document.write(`Probability to get sum ${sum_of_dices} with ${number_of_dices} dices is ${(counter / 10000 * 100).toFixed(2)}%`);
--- Module 1/JS_8.js
+++ Module 1/JS_8.js
@@ -5,33 +5,14 @@ let start_year, end_year;
 start_year = prompt("Input start year");
 end_year = prompt("Input end year");
 
-/*
-for (let i = start_year; i <= end_year; i++) {    
-    if (i % 4 === 0 && i % 100 != 0) {
-        //document.write(`The year ${input_year} is a leap year`);
-
-        li.innerText = i;
-        ulist.appendChild(li);
-    }
-    else if (i % 100 === 0 && i % 400 === 0) {
-        //document.write(`The year ${input_year} is a leap year`);
-        
-        li.innerText = i;
-        ulist.appendChild(li);
-    }
-}
-
-document.write(ulist)
-*/
-document.write("<ul>");
+var ul = document.createElement("ul")
 for (let i = start_year; i <= end_year; i++) {    
     
-    if (i % 4 === 0 && i % 100 != 0) {
-        document.write("<li>", i, "</li>");
-    }
-    else if (i % 100 === 0 && i % 400 === 0) {
-        document.write("<li>", i, "</li>");
+    if ((i % 4 === 0 && i % 100 != 0) || (i % 100 === 0 && i % 400 === 0)) {
+        ul.innerHTML += `<li>${i}</li>`;
     }
 }
-document.write("</ul>")
+
+document.body.appendChild(ul);
+
 
