59ebba36eb9a4f3577ba174bab39a3ef25531f5b
MKRadius <caomanhkhai110@gmail.com>
2022-11-29 23:09:29 +0200

    Exercise

---
+++ Module 3/t1/1.js
@@ -0,0 +1,9 @@
+const ul = document.querySelector('#target');
+
+const li =
+            `<li>First item</li>
+            <li>Second item</li>
+            <li>Third item</li>`;
+
+ul.innerHTML = li;
+ul.classList.add("my-list");
