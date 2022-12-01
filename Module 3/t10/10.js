56a48015b3383dc6cc1b59d08960c7338553afc4
MKRadius <caomanhkhai110@gmail.com>
2022-12-01 05:34:58 +0200

    Exercise

--- Module 3/t10/10.js
+++ Module 3/t10/10.js
@@ -3,6 +3,6 @@ document.getElementById("source").addEventListener("submit", (e) => {
 
     const fname = document.querySelector("input[name='firstname']").value;
     const lname = document.querySelector("input[name='lastname']").value;
+    
     document.getElementById("target").innerText = `Your name is ${fname} ${lname}`;
-
 });
